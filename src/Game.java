5b5a68407a078dfa27aa99cd69633d6e33808074
Ethan Gonzalez <112298070+EthanGon@users.noreply.github.com>
2026-03-12 18:43:33 -0400

    Reverted Changes + Include Chase Method Done By Ariel

--- src/Game.java
+++ src/Game.java
@@ -6,26 +6,37 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Toolkit;
+import java.awt.Frame;
 
 
 public class Game extends Applet implements Runnable, KeyListener {
-	
 	private Thread thread;
 	private Camera mainCam;
 	private Image doubleBuffer;
 	private ChunkManager worldManager;
-	private Player player = new Player(0,0,64,64);
+	private Player player;//
 	private Enemy e1 = new Enemy(0,0);
+	private  enum game_state {paused , playing};
+	game_state curr_state ;
+	
+	private Image pause_img;
+	private Monsters m = new Monsters(0,0, 64,64);
 	
 	
 	public void init() {
+
+
+		player = new Player(this, 0, 0, 64, 64);//
+		
 		mainCam = new Camera();
 		mainCam.setPosition(player.x, player.y);
 		worldManager = new ChunkManager();
 		
 		this.setSize(1280, 720);
-		
-		
+		pause_img = Toolkit.getDefaultToolkit().getImage("dark_paused.png");
+
+		curr_state = game_state.playing;
 		thread = new Thread(this);
 		thread.start();
 		
@@ -44,7 +55,27 @@ public class Game extends Applet implements Runnable, KeyListener {
 		// DRAW OBJECTS BELOW HERE (ensures world is drawn before objects)
 		g.setColor(Color.red);
 		player.draw(g);
+		m.draw(g);
 		e1.draw(g);
+		
+		
+	/*****	alternate pause screen using a image 	*****
+	 ***** comment out showUpgradeMenu() in pause()	*****
+	 *****  before testing this out ,               *****   \/
+	 
+		if(curr_state == game_state.paused) {
+			g.setColor (Color.green );
+			g.drawImage(pause_img, (getWidth() /2 ) -240,   (getHeight() /2)-250 , 479,499,null);
+
+			Rect pause_rect = new Rect ( (getWidth() /2 ) -240 , (getHeight() /2)-250 , 480,500);
+		
+			pause_rect.set_show_origin(false);
+			pause_rect.draw(g);
+		
+		}
+
+		**/
+
 		// DRAW OBJECTS ABOVE HERE 
 		
 		
@@ -57,10 +88,12 @@ public class Game extends Applet implements Runnable, KeyListener {
 	public void run() {
 		
 		while (true) {
-			
-			movePlayer();
-			updateCameraPosition();
-			checkCameraBounds();
+			if(curr_state!= game_state.paused) {
+				movePlayer();
+				m.chase(player);
+				updateCameraPosition();
+				checkCameraBounds();
+			}
 			
 			repaint();
 			
@@ -77,26 +110,87 @@ public class Game extends Applet implements Runnable, KeyListener {
 		
 	}
 
+		//WF 3/5/26 pause functions that will pause the game for the upgradeMenu
+//	public void pauseGame() {
+//		paused = true;
+//	}
+	
+	public void unpause() {
+		
+		curr_state = game_state.playing;
+		System.out.println("game unpaused");	
+
+	}
+	
+	public boolean check_paused() {
+		return (curr_state== game_state.paused );
+		
+	}
+	
+
+	
+	public void showUpgradeMenu() {
+
+	    UpgradeMenu menu = new UpgradeMenu(
+	        null,
+	        player.getHealthUpgrade(),
+	        player.getStrengthUpgrade(),
+	        player.getDefenseUpgrade(),
+	        this::unpause
+	    );
+
+	    menu.setVisible(true);
+	}/////WF
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
+		
+		if ((key== KeyEvent.VK_ESCAPE) ) {
+			if (curr_state == game_state.playing) {
+			pause();
+			return;
+			}
+			unpause();
+			return;
+		}
+		
 		
 		if (key == KeyEvent.VK_D) {player.RIGHT = true;}
 		if (key == KeyEvent.VK_A) {player.LEFT = true;}
 		if (key == KeyEvent.VK_W) {player.UP = true;}
 		if (key == KeyEvent.VK_S) {player.DOWN = true;}
 		
-	
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
+		
 		int key = e.getKeyCode();
 		
+		if ( curr_state == game_state.paused ) 
+			return;
+	
 		if (key == KeyEvent.VK_D) player.RIGHT = false;
 		if (key == KeyEvent.VK_A) player.LEFT = false;
 		if (key == KeyEvent.VK_W) player.UP = false;
 		if (key == KeyEvent.VK_S) player.DOWN = false;
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
+
+	private  synchronized void pause() {
+		
+		 curr_state = game_state.paused ;
+			 System.out.println("game paused");
+			 player.RIGHT = false;
+			 player.LEFT = false;
+			 player.UP = false;
+			 player.DOWN = false;
+			showUpgradeMenu();
+		
 		
 	}
 
@@ -112,6 +206,7 @@ public class Game extends Applet implements Runnable, KeyListener {
 	}
 	
 	public void movePlayer() {
+
 		if (player.RIGHT) player.x += player.moveSpeed;
 		if (player.LEFT) player.x -= player.moveSpeed;
 		if (player.UP) player.y -= player.moveSpeed;
@@ -142,9 +237,7 @@ public class Game extends Applet implements Runnable, KeyListener {
 		Font newFont = g.getFont().deriveFont(25f);
 		g.setFont(newFont);
 		
-		g.drawString("Move: WASD", 10, 40);
+		g.drawString("Move: WASD\tESC to pause", 10, 40);
 	}
 	
-	@Override
-	public void keyTyped(KeyEvent e) {}
 }
