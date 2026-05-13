03b26c4e9413b26922ea218704da8ba0a2c4f702
wilsonfajardo54 <wfajardo54@yahoo.com>
2026-05-12 20:57:05 -0400

    Refactor Mob class to extend Sprite
    Updated Mob class to extend Sprite class, allowing for image loading and position management.

--- src/Mob.java
+++ src/Mob.java
@@ -1,9 +1,13 @@
 import java.awt.Graphics;
 
-public abstract class Mob {
-	int x;
-	int y;
+public abstract class Mob extends Sprite {
+	//int x;
+	//int y;
 	
+	
+	public Mob(String name, int x, int y, int w, int h, int direction, String[] pose) {
+        super(name, x, y, w, h, direction, pose);
+    }
 	
 	/*  With how this is designed this needs to be written before g.draw() is called
 	 * 
@@ -14,6 +18,13 @@ public abstract class Mob {
 		TODO (not needed): Could update this code so this line isn't repeated
 		
 	 * */
-	public abstract void draw(Graphics g);
+	//public abstract void draw(Graphics g);
 
 }
+
+
+/* Extended Mob class to Sprite class.
+* With this we can load images of player and enemies.
+* Player position is now loaded into Sprite class which extends Rect class. 
+* WF
+*/
